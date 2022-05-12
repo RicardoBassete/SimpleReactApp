b940bf9214af0eaa47d6d47ec4c83ec5f7199bf2
Ricardo Gonçalves Bassete <ricardogbassete@gmail.com>
2022-05-12 13:02:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+import {API_KEY} from '../.env'
 
 import SearchBar from "./components/search_bar";
 
-import {API_KEY} from '../.env'
-
 const App = () => {
+  const [videos, setVideos] = useState([])
+  useEffect(() => {
+    YTSearch({key: API_KEY, term: 'minecraft'}, videos => {
+      setVideos(videos)
+    })
+  }, [])
+
+
   return (
     <div>
       <SearchBar/>
